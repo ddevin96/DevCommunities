43459b08469faad5a572c2a1d5c7d99ec2a28cc6
ddevin96 <danieledevinco1996@gmail.com>
2023-10-30 11:52:13 +0100

    update pipe

---
+++ src/notebook/orb.sh
@@ -0,0 +1,6 @@
+# ssh orb "
+# cd /Users/ddevin/Documents/MoCHy
+# bash run_exact_par.sh /Users/ddevin/Documents/vscode/DevCommunities/randoms/randomPIPE.hg"
+ssh orb "
+cd /Users/ddevin/Documents/MoCHy
+bash run_approx_ver2_par.sh /Users/ddevin/Documents/vscode/DevCommunities/randoms/randomPIPE.hg"
--- src/notebook/rewrite.py
+++ src/notebook/rewrite.py
@@ -15,7 +15,7 @@ path = str(sys.argv[1])
 num_edges = int(sys.argv[2])
 # print("path: ", path)
 # print("num_edges: ", num_edges)
-porcodio = 0
+# porcodio = 0
 # open file and read the content in a list
 w_file = open("/Users/ddevin/Documents/vscode/DevCommunities/randoms/randomPIPE.hg", "w")
 with open(path, "r") as f:
@@ -28,10 +28,10 @@ with open(path, "r") as f:
         ids = list(map(index, ids))
         new_line = ','.join(map(str, ids))
         w_file.write(new_line + '\n')
-        porcodio += 1
-        if porcodio == num_edges:
-            # print("counter: ", porcodio)
-            # print("num_edges: ", num_edges)
-            break
+        # porcodio += 1
+        # if porcodio == num_edges:
+        #     # print("counter: ", porcodio)
+        #     # print("num_edges: ", num_edges)
+        #     break
 
 w_file.close()
